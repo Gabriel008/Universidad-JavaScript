7a6bca387d349fc4198b1ec591a01695100ef272
Gabriel Valenzuela <gabriel.valenzuela001@gmail.com>
2023-11-25 14:52:53 -0300

    Universidad JavaScript - De Cero a Experto Cap 8-13 25112023

---
+++ Cap 12- Ejemplo de Reglas de Variables en JavaScript.js
@@ -0,0 +1,14 @@
+//Reglas de nombres de variables
+//letras, $, _, posteriormente se pueen usar numeros
+let $miNumero1 = 10;
+console.log($miNumero1);
+
+let _miNumero2 = 20;
+console.log(_miNumero2);
+
+let miNumero3 = 30;
+console.log(miNumero3);
+
+
+//JavaScript es sensible a May/min
+//console.log(MiNumero3);
